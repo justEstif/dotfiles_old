b31b3c4da3ee2c08cd9ad5a036be73fc0599421c
Estifanos <estifanosbeyene21@gmail.com>
2022-09-16 08:55:55 -0400

    feat; add shell

--- config/fish/alias.fish
+++ config/fish/alias.fish
@@ -2,6 +2,9 @@
 alias lf='ranger_cd' # file manager
 alias lz='lazygit'  # git client
 
+# kill all node apps
+alias killnode="killall -9 node"
+
 ## exa
 alias l="exa -lba" # ls
 alias ls="exa" # ls
