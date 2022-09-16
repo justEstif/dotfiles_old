50687b3a30eaa3f34ad87c0759ed039ad1859e1c
Estifanos <estifanosbeyene21@gmail.com>
2022-09-16 03:44:58 -0400

    feat: add exa alias to fish

--- config/fish/alias.fish
+++ config/fish/alias.fish
@@ -1,2 +1,9 @@
-alias lf='ranger_cd'
-alias lz='lazygit'
+## app aliases
+alias lf='ranger_cd' # file manager
+alias lz='lazygit'  # git client
+
+## exa
+alias l="exa -lba" # ls
+alias ls="exa" # ls
+alias la='exa -albF --git' # list, size, type, git
+
