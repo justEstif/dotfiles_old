820d73aed97229d133bd91102d3420edc065ec94
Estifanos <estifanosbeyene21@gmail.com>
2022-09-23 04:06:45 -0400

    feat: remove autocmd to run packer on save

--- config/nvim/lua/packer_init.lua
+++ config/nvim/lua/packer_init.lua
@@ -12,14 +12,6 @@ if fn.empty(fn.glob(install_path)) > 0 then
 	vim.o.runtimepath = vim.fn.stdpath("data") .. "/site/pack/*/start/*," .. vim.o.runtimepath
 end
 
--- Autocommand that reloads neovim whenever you save the packer_init.lua file
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost packer_init.lua source <afile> | PackerSync
-  augroup end
-]])
-
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
