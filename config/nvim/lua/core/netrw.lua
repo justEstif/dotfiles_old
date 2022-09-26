fe37f025f13ae0072b8a427f7fc6c4958f117d3e
Estifanos <estifanosbeyene21@gmail.com>
2022-09-26 08:37:26 -0400

    feat: add more keybinds

--- config/nvim/lua/core/netrw.lua
+++ config/nvim/lua/core/netrw.lua
@@ -3,6 +3,7 @@
 ----------------------------------------------------------
 
 local g = vim.g
+local autocmd = vim.api.nvim_create_autocmd -- Create autocommand
 
 local options_append = {
 	netrw_keepdir = 0, --Keep the current directory and the browsing directory synced
@@ -17,7 +18,7 @@ for k, v in pairs(options_append) do
 	g[k] = v
 end
 
-vim.api.nvim_create_autocmd("filetype", {
+autocmd("filetype", {
 	pattern = "netrw",
 	desc = "Better mappings for netrw",
 	callback = function()
@@ -25,11 +26,34 @@ vim.api.nvim_create_autocmd("filetype", {
 			vim.keymap.set("n", lhs, rhs, { remap = true, buffer = true })
 		end
 
-    -- NOTE use terminal for other things
 		bind("H", "u") -- preview dir
 		bind("h", "-^") -- go up
 		bind("l", "<CR>") -- open file or dir
 		bind(".", "gh") -- toggle dotfiles
 		bind("<leader>dd", ":Lexplore<CR>") -- close if open
+
+		-- Marks
+		bind("<Tab>", "mf") -- add mark
+		bind("<S-Tab>", "mf") -- remove mark
+		bind("<leader><tab>", "mu") -- remove all marks
+
+		-- Files
+		bind("ff", ":!touch ") -- create file
+		bind("fF", ":!mkdir -p ") -- create folder
+		bind("fe", "R") -- Rename
+		bind("FF", "!rm -r ") -- Delete
+		bind("fc", "mc") -- copy
+		bind("fC", "mtmc")
+		bind("fx", "mm") -- move marked files
+		bind("fX", "mtmm")
+		bind("f;", "mx") -- run command
+
+		bind("fl", ' :echo join(netrw#Expose("netrwmarkfilelist"), "\n")<CR>') -- show list of marked files
+		bind("fq", [[:echo 'Target:' . netrw#Expose("netrwmftgt")<CR>]]) -- show the target directory,
+		bind("fd", "mtfq")
+
+		bind("bb", "mb") -- To create a bookmark.
+		bind("bd", "mB") -- To remove the most recent bookmark.
+		bind("bl", "gb") -- To jump to the most recent bookmark.
 	end,
 })
