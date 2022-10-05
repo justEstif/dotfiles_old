cef9c30652e9434bbcce94a97cda6022b442b521
Estifanos <estifanosbeyene21@gmail.com>
2022-10-05 16:19:02 -0400

    feat: add autocmd for adding dir to file list

--- config/nvim/lua/core/autocmds.lua
+++ config/nvim/lua/core/autocmds.lua
@@ -62,3 +62,17 @@ augroup InitNetrw
   autocmd VimEnter * if expand("%") == "" | edit . | endif
 augroup END
 ]])
+
+-- Add file directory to Vim path
+-- source: <https://vim.fandom.com/wiki/Set_working_directory_to_the_current_file>
+vim.cmd([[
+let s:default_path = escape(&path, '\ ') " store default value of 'path'
+" Always add the current file's directory to the path and tags list if not
+" already there. Add it to the beginning to speed up searches.
+autocmd BufRead *
+      \ let s:tempPath=escape(escape(expand("%:p:h"), ' '), '\ ') |
+      \ exec "set path-=".s:tempPath |
+      \ exec "set path-=".s:default_path |
+      \ exec "set path^=".s:tempPath |
+      \ exec "set path^=".s:default_path
+]])
