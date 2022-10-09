26bd19e5b8489432113d79597883354bbeb899c9
Estifanos <estifanosbeyene21@gmail.com>
2022-10-09 13:21:08 -0400

    feat: add tmux aliases

--- config/fish/alias.fish
+++ config/fish/alias.fish
@@ -10,3 +10,6 @@ alias l="exa -lba" # ls
 alias ls="exa" # ls
 alias la='exa -albF --git' # list, size, type, git
 
+# tmux alias
+alias tmuxq="tmux kill-session" # close terminal
+alias tmuxqa="tmux kill-server" # close all terminal
