bf403ad4b3f13b5805e25c429be8e2bce6c45494
Estifanos <estifanosbeyene21@gmail.com>
2022-10-20 20:30:51 -0400

    feat: add more nvim tree settings

--- config/nvim/lua/plugins/nvim-tree.lua
+++ config/nvim/lua/plugins/nvim-tree.lua
@@ -47,6 +47,51 @@ nvim_tree.setup({
 			},
 		},
 	},
+	actions = {
+		use_system_clipboard = true,
+		change_dir = {
+			enable = true,
+			global = false,
+			restrict_above_cwd = false,
+		},
+		expand_all = {
+			max_folder_discovery = 300,
+			exclude = {},
+		},
+		file_popup = {
+			open_win_config = {
+				col = 1,
+				row = 1,
+				relative = "cursor",
+				border = "shadow",
+				style = "minimal",
+			},
+		},
+		git = {
+			enable = true,
+			ignore = true,
+			show_on_dirs = true,
+			timeout = 400,
+		},
+		open_file = {
+			quit_on_open = true,
+			resize_window = true,
+			window_picker = {
+				enable = true,
+				chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+				exclude = {
+					filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
+					buftype = { "nofile", "terminal", "help" },
+				},
+			},
+		},
+		remove_file = {
+			close_window = true,
+		},
+	},
+	filters = {
+		custom = { "^.git$" },
+	},
 	diagnostics = {
 		enable = true,
 		show_on_dirs = true,
