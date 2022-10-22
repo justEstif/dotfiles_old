3ea887566e55035fbf1ee01183c20176c3f94226
Estifanos <estifanosbeyene21@gmail.com>
2022-10-21 20:43:09 -0400

    feat: fix keybinds

--- config/nvim/lua/core/commands.lua
+++ config/nvim/lua/core/commands.lua
@@ -5,5 +5,3 @@
 local command = function(command, target)
 	vim.api.nvim_create_user_command(command, target, {})
 end
-
-command("Gpush", "G push") -- add fugitive push
--- config/nvim/lua/plugins/nvim-cmp.lua
+++ config/nvim/lua/plugins/nvim-cmp.lua
@@ -12,6 +12,11 @@ if not luasnip_status_ok then
 	return
 end
 
+local check_backspace = function()
+	local col = vim.fn.col(".") - 1
+	return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
+end
+
 local kind_icons = {
 	Text = "",
 	Method = "",
@@ -58,15 +63,44 @@ cmp.setup({
 
 	-- Key mapping
 	mapping = {
-		["<Tab>"] = cmp.mapping.select_next_item(), -- scroll using tab
-		["<S-Tab>"] = cmp.mapping.select_prev_item(), -- scroll using s-tab
-		["<C-b>"] = cmp.mapping.scroll_docs(-4), -- scroll docs
-		["<C-f>"] = cmp.mapping.scroll_docs(4), -- scroll docs
-		["<C-Space>"] = cmp.mapping.complete(), -- show using <C-space>
-		["\\"] = cmp.mapping.close(), -- close using \
-		["<CR>"] = cmp.mapping.confirm({ -- using enter to pick
-			behavior = cmp.ConfirmBehavior.Replace,
-			select = true,
+		["<C-k>"] = cmp.mapping.select_prev_item(),
+		["<C-j>"] = cmp.mapping.select_next_item(),
+		["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
+		["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
+		["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+		["<C-y>"] = cmp.config.disable,
+		["\\"] = cmp.mapping({
+			i = cmp.mapping.abort(),
+			c = cmp.mapping.close(),
+		}),
+		["<CR>"] = cmp.mapping.confirm({ select = true }),
+		["<Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif luasnip.expandable() then
+				luasnip.expand()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			elseif check_backspace() then
+				fallback()
+			else
+				fallback()
+			end
+		end, {
+			"i",
+			"s",
+		}),
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, {
+			"i",
+			"s",
 		}),
 	},
 
@@ -93,5 +127,14 @@ cmp.setup({
 		{ name = "path" },
 		{ name = "buffer" },
 		{ name = "nvim_lua" },
+	},
+	confirm_opts = {
+		behavior = cmp.ConfirmBehavior.Replace,
+		select = false,
+	},
+	window = {
+		documentation = {
+			border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+		},
 	},
 })
