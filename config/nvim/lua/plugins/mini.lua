90b487f2de26797095ab3af0728751e2a97bd1f4
Estifanos <estifanosbeyene21@gmail.com>
2022-11-28 13:08:39 -0500

    feat: use mini surround and ai

--- config/nvim/lua/plugins/mini.lua
+++ config/nvim/lua/plugins/mini.lua
@@ -56,3 +56,43 @@ mini_jump.setup({
 	},
 	idle_stop = 1000,
 })
+
+-- Mini ai
+local status_ai, mini_ai = pcall(require, "mini.ai")
+if not status_ai then
+	print("mini.ai error")
+	return
+end
+
+mini_ai.setup({
+	search_method = "cover_or_nearest",
+})
+
+-- Mini surround configuration file
+local status_surround, mini_surround = pcall(require, "mini.surround")
+if not status_surround then
+	print("mini.surround error")
+	return
+end
+
+-- Replace vim-surround:
+mini_surround.setup({
+	mappings = {
+		add = "ys",
+		delete = "ds",
+		find = "",
+		find_left = "",
+		highlight = "",
+		replace = "cs",
+		update_n_lines = "",
+		-- Add this only if you don't want to use extended mappings
+		suffix_last = "",
+		suffix_next = "",
+	},
+	search_method = "cover_or_nearest",
+})
+-- Remap adding surrounding to Visual mode selection
+vim.api.nvim_del_keymap("x", "ys")
+vim.api.nvim_set_keymap("x", "S", [[:<C-u>lua MiniSurround.add('visual')<CR>]], { noremap = true })
+-- Make special mapping for "add surrounding for line"
+vim.api.nvim_set_keymap("n", "yss", "ys_", { noremap = false })
--- config/nvim/notes.md
+++ config/nvim/notes.md
@@ -1,2 +1,4 @@
-- Still have some issues with the working directory setup
 - Markdown highlighting
+
+<https://www.reddit.com/r/vim/comments/1z93ww/append_to_a_text_object/>
+
