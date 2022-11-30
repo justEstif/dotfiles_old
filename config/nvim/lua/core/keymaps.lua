ef7ddfb6c6de5f828362387c0b190e67a8f05da6
Estifanos <estifanosbeyene21@gmail.com>
2022-11-30 13:25:11 -0500

    feat: change keybind

--- config/nvim/lua/core/keymaps.lua
+++ config/nvim/lua/core/keymaps.lua
@@ -25,8 +25,10 @@ vim.g.mapleader = ","
 
 -- Override default
 map("v", "p", '"_dP', { desc = "Keep the yanked text when pasting in visual  mode" })
-map("v", "<", "<gv", { desc = "Stay in indent mode" })
-map("v", ">", ">gv", { desc = "Stay in indent mode" })
+map("n", ">", ">>", { desc = "Indent" })
+map("n", "<", "<<", { desc = "Indent" })
+map("v", "<", "<gv", { desc = "Indent" })
+map("v", ">", ">gv", { desc = "Indent" })
 map("x", "@", '":norm @" . getcharstr() . "<cr>"', { expr = true, desc = "Appy macro on visual range" })
 map("t", "<Esc>", [[<C-\><C-n>]], { desc = "Esc exit terminal" })
 map("n", "<Esc>", ":nohl<CR>", { desc = "Clear search highlights" })
@@ -35,10 +37,10 @@ map("n", "<Esc>", ":nohl<CR>", { desc = "Clear search highlights" })
 map("n", "<leader>q", ":%bdelete<CR>", { desc = "Close all buffers" })
 map("n", "<leader>b", ":b <C-z>", { desc = "List open buffers" })
 map("n", "<leader>rr", ":so %<cr>", { desc = "Reload config" })
-map("n", "<leader>f", ":NvimTreeToggle<CR>", { desc = "Open Nvim tree" })
 
 -- Plugins shortcuts
 map("n", "<leader><leader>", ":Telescope find_files<CR>", { desc = "Telescope: find files" })
-map("n", "<leader>s", ":Telescope live_grep<CR>", { desc = "Telescope: live grep" })
-map("n", "<leader>d", ":Telescope cder<cr>", { desc = "Telescope: project" })
+map("n", "<leader>c", ":Telescope cder<cr>", { desc = "Telescope: project" })
+map("n", "<leader>d", ":NvimTreeToggle<CR>", { desc = "Open Nvim tree" })
 map("n", "<leader>g", ":tab G<cr>", { desc = "Fugitive: open" })
+map("n", "<leader>s", ":Telescope live_grep<CR>", { desc = "Telescope: live grep" })
