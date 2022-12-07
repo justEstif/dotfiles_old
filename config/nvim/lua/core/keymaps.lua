606918c204327986c5894cac0cab47e7ace27876
Estifanos <estifanosbeyene21@gmail.com>
2022-12-07 14:12:03 -0500

    feat: lsp keymaps

--- config/nvim/lua/core/keymaps.lua
+++ config/nvim/lua/core/keymaps.lua
@@ -22,19 +22,8 @@ map("n", "<Esc>", ":nohl<CR>", { desc = "Clear search highlights" })
 map("n", "<leader>q", ":%bdelete<CR>", { desc = "Close all buffers" })
 map("n", "<leader>b", ":b <C-z>", { desc = "List open buffers" })
 
--- Lsp
-map("n", "gd", "<cmd>Telescope lsp_definitions<CR>")
-map("n", "gt", "<cmd>Telescope lsp_type_definitions<cr>")
-map("n", "gr", "<cmd>Telescope lsp_references<CR>")
-map("n", "gh", "<cmd>lua vim.lsp.buf.hover()<CR>")
-map("n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>")
-map("n", "gL", "<cmd>Telescope diagnostics<CR>")
-map("n", "gR", "<cmd>lua vim.lsp.buf.rename()<CR>")
-map("n", "gca", "<cmd>lua vim.lsp.buf.code_action()<CR>")
-map("n", "=", "<cmd>lua vim.lsp.buf.format{async=true}<CR>")
-
 -- Plugins shortcuts
 map("n", "<leader>d", ":NvimTreeToggle<CR>", { desc = "Open Nvim tree" })
-map("n", "<leader>g", ":tab G<cr>", { desc = "Fugitive: open" })
+map("n", "<leader>g", ":G<cr>", { desc = "Fugitive: open" })
 map("n", "<leader>f", ":Telescope find_files<CR>", { desc = "Telescope: find files" })
 map("n", "<leader>s", ":Telescope live_grep<CR>", { desc = "Telescope: live grep" })
