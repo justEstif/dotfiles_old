85e5b15d7dc831295011397d0b30f5d87d973421
Estifanos <estifanosbeyene21@gmail.com>
2022-12-09 16:44:31 -0500

    feat: add empty file keymap

--- config/nvim/lua/plugins/mini/starter.lua
+++ config/nvim/lua/plugins/mini/starter.lua
@@ -19,9 +19,10 @@ mini_starter.setup({
 		[[\_\ \/ \___|\___/ \_/ |_|_| |_| |_|]],
 		[[───────────────────────────────────]],
 	}, "\n"),
-	query_updaters = "qQfF",
+	query_updaters = "eEfFqQ",
 	items = {
 		{ action = "NvimTreeOpen", name = "F: Open File Tree", section = "Plugins" },
+		{ action = "enew", name = "E: New Buffer", section = "Builtin actions" },
 		{ action = "qall!", name = "Q: Quit Neovim", section = "Builtin actions" },
 	},
 })
