3a6fa043ef5e4b91356e43949d9e5b285dfc4054
Estifanos <estifanosbeyene21@gmail.com>
2022-12-14 09:25:26 -0500

    refactor: move workspace config to file

--- config/nvim/after/ftplugin/NvimTree.lua
+++ config/nvim/after/ftplugin/NvimTree.lua
@@ -1,3 +1,9 @@
 vim.b.miniindentscope_disable = true
 
-vim.b.ministatusline_config = { content = { active = function() return 'NvimTree' end } }
+vim.b.ministatusline_config = {
+	content = {
+		active = function()
+			return "NvimTree"
+		end,
+	},
+}
