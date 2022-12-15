6e9c5634391c42edad9c55a77472b159747e46fb
Estifanos <estifanosbeyene21@gmail.com>
2022-12-15 12:39:20 -0500

    feat(null-ls): add gitsigns code actions

--- config/nvim/lua/plugins/null-ls.lua
+++ config/nvim/lua/plugins/null-ls.lua
@@ -1,12 +1,13 @@
 local status, null_ls = pcall(require, "null-ls")
 if not status then
-  print("null-ls error")
+	print("null-ls error")
 	return
 end
 
 local formatting = null_ls.builtins.formatting
 local completion = null_ls.builtins.completion
 local diagnostics = null_ls.builtins.diagnostics
+local code_actions = null_ls.builtins.code_actions
 
 null_ls.setup({
 	debug = false,
@@ -15,6 +16,7 @@ null_ls.setup({
 		diagnostics.jsonlint, -- json
 		formatting.rustywind, -- tailwind
 		formatting.stylua, -- lua
+		code_actions.gitsigns,
 		completion.spell.with({
 			filetypes = { "markdown", "gitcommit" },
 		}),
