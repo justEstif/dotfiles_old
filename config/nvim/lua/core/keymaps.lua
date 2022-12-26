a011b1288567bc5ee45d5826668df6c57c136899
Estifanos <estifanosbeyene21@gmail.com>
2022-12-25 19:00:21 -0500

    feat: add keymaps for mind and tabs

--- config/nvim/lua/core/keymaps.lua
+++ config/nvim/lua/core/keymaps.lua
@@ -15,17 +15,21 @@ map("n", "<C-l>", function()
 	vim.cmd.nohlsearch()
 	vim.cmd.echo()
 end, { desc = "clear highlights" })
+map("n", "<leader>ot", ":tabnew<cr>", { desc = "open new tab" })
+map("n", "<leader>ct", ":tabclose<cr>", { desc = "close tab" })
 
 -- Utils
-map("n", "gx", [[:lua require("core.utils").go_to_url()<cr>]], { desc = "go to url under cursor" })
 map("n", "<leader>r", [[:lua require("core.utils").run_file()<cr>]], { desc = "run current file" })
 
--- Plugin shortcuts
-map("n", "<leader>f", ":NvimTreeToggle<CR>", { desc = "Open Nvim tree" })
-map("n", "<leader>g", ":G<cr>", { desc = "Open fugitive" })
+-- Mind
+map("n", "<leader>om", ":MindOpenProject<cr>", { desc = "open mind project" })
+map("n", "<leader>cm", ":MindClose<cr>")
 
 -- Telescope
 map("n", "<leader>b", ":Telescope buffers<cr>", { desc = "List open buffers" })
 map("n", "<leader>s", ":Telescope live_grep<CR>", { desc = "Search in cwd" })
 map("n", "<leader><leader>", ":Telescope find_files<CR>", { desc = "Find files in cwd" })
 map("n", "z=", ":Telescope spell_suggest<CR>", { desc = "Spell suggest keybind" })
+
+map("n", "<leader>f", ":NvimTreeToggle<CR>", { desc = "Open Nvim tree" })
+map("n", "<leader>g", ":G<cr>", { desc = "Open fugitive" })
