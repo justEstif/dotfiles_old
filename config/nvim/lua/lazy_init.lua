e0ce0b83ce84fa582d305f785a1d7e0d16392c9f
Estifanos <estifanosbeyene21@gmail.com>
2022-12-27 19:07:43 -0500

    refactor: changes to match lazy setup

--- config/nvim/lua/lazy_init.lua
+++ config/nvim/lua/lazy_init.lua
@@ -19,45 +19,118 @@ if not status_ok then
 end
 
 lazy.setup({
-	{ "nvim-lua/plenary.nvim" },
-	{ "nvim-treesitter/nvim-treesitter" }, -- treesitter
-	{ "windwp/nvim-ts-autotag" }, -- autoclose and autorename html tags
-	{ "JoosepAlviste/nvim-ts-context-commentstring" }, -- jsx comments support
+	"nvim-lua/plenary.nvim",
+	{
+		"nvim-treesitter/nvim-treesitter",
+		dependencies = {
+			"JoosepAlviste/nvim-ts-context-commentstring", -- jsx comments support
+		},
+		config = function()
+			require("plugins.nvim-treesitter")
+		end,
+	},
+	{ "windwp/nvim-ts-autotag", config = true }, -- autoclose and autorename html tags
 	{
 		"neovim/nvim-lspconfig", -- nvim native lsp
-		"williamboman/mason.nvim", -- Installer for external tools
-		"williamboman/mason-lspconfig.nvim", -- mason extension for lspconfig
-		"jose-elias-alvarez/typescript.nvim", -- add features to tsserver config
+		dependencies = {
+			"williamboman/mason.nvim", -- Installer for external tools
+			"williamboman/mason-lspconfig.nvim", -- mason extension for lspconfig
+			"jose-elias-alvarez/typescript.nvim", -- add features to tsserver config
+		},
+		config = function()
+			require("plugins.lsp")
+		end,
 	},
 	{
 		"hrsh7th/nvim-cmp", -- cmp plugin
-		"hrsh7th/cmp-nvim-lsp", -- lsp cmp
-		"hrsh7th/cmp-path", -- path cmp
-		"hrsh7th/cmp-buffer", -- buffer cmp
-		"saadparwaiz1/cmp_luasnip", -- snippets cmp
-		"L3MON4D3/LuaSnip", -- snippets engine
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp", -- lsp cmp
+			"hrsh7th/cmp-path", -- path cmp
+			"hrsh7th/cmp-buffer", -- buffer cmp
+			"saadparwaiz1/cmp_luasnip", -- snippets cmp
+			{
+				"L3MON4D3/LuaSnip", -- snippets engine
+				config = function()
+					require("plugins.luasnip")
+				end,
+			},
+		},
+		config = function()
+			require("plugins.nvim-cmp")
+		end,
 	},
-	{ "kyazdani42/nvim-tree.lua" }, -- file explorer
+	{ -- file explorer
+		"kyazdani42/nvim-tree.lua",
+		config = function()
+			require("plugins.nvim-tree")
+		end,
+	},
 	{
 		"nvim-telescope/telescope.nvim",
 		branch = "0.1.x",
 		requires = { "kyazdani42/nvim-web-devicons", "nvim-lua/plenary.nvim" },
+		dependencies = {
+			{
+				"tiagovla/scope.nvim", -- scope buffers to tabs
+				config = function()
+					require("plugins.scope")
+				end,
+			},
+		},
+
+		config = function()
+			require("plugins.telescope")
+		end,
 	},
 
 	{ "tpope/vim-fugitive" }, -- git client
-	{ "lewis6991/gitsigns.nvim", requires = "nvim-lua/plenary.nvim" }, -- git signs
+	{
+		"lewis6991/gitsigns.nvim",
+		requires = "nvim-lua/plenary.nvim",
+		config = function()
+			require("plugins.gitsigns")
+		end,
+	}, -- git signs
 
-	{ "jose-elias-alvarez/null-ls.nvim" }, -- formatter
-	{ "echasnovski/mini.nvim" }, -- pairs/indent/comment
+	{
+		"jose-elias-alvarez/null-ls.nvim",
+		config = function()
+			require("plugins.null-ls")
+		end,
+	}, -- formatter
+	{
+		"echasnovski/mini.nvim",
+		config = function()
+			require("plugins.mini")
+		end,
+	}, -- pairs/indent/comment
 	{ "wakatime/vim-wakatime" }, -- wakatime
-	{ "akinsho/toggleterm.nvim" }, -- toggle term
-	{ "tiagovla/scope.nvim" }, -- scope buffers to tabs
+	{
+		"akinsho/toggleterm.nvim",
+		config = function()
+			require("plugins.toggleterm")
+		end,
+	}, -- toggle term
 	{
 		"phaazon/mind.nvim",
 		branch = "v2.2",
 		requires = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("plugins.mind")
+		end,
 	},
-
-	{ "kyazdani42/nvim-web-devicons" }, -- icons
-	{ "catppuccin/nvim", as = "catppuccin" }, -- theme
+	{
+		"kyazdani42/nvim-web-devicons",
+		config = function()
+			require("plugins.devicons")
+		end,
+	}, -- icons
+	{
+		"catppuccin/nvim",
+		as = "catppuccin",
+		lazy = false,
+		config = function()
+			require("plugins.theme")
+		end,
+	}, -- theme
 })
