f62f42367e1324bb7c4af16fb9e7569537d28abd
Estifanos <estifanosbeyene21@gmail.com>
2023-01-11 16:46:24 -0500

    feat: improve mini statusline style

--- config/nvim/lua/plugins/mini/statusline.lua
+++ config/nvim/lua/plugins/mini/statusline.lua
@@ -4,4 +4,20 @@ if not status then
 	return
 end
 
-statusline.setup({})
+statusline.setup({
+	content = {
+		active = function()
+			local diagnostics = statusline.section_diagnostics({ trunc_width = 75 })
+			local filename = statusline.section_filename({ trunc_width = 140 })
+			local fileinfo = statusline.section_fileinfo({ trunc_width = 320 })
+
+			return statusline.combine_groups({
+				{ hl = "MiniStatuslineDevinfo", strings = { diagnostics } },
+				"%<", -- Mark general truncate point
+				{ hl = "MiniStatuslineFilename", strings = { filename } },
+				"%=", -- End left alignment
+				{ hl = "MiniStatuslineFileinfo", strings = { fileinfo } },
+			})
+		end,
+	},
+})
