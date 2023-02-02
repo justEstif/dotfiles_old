fbded2125386fff0407d2954f32ad71f3f9b86f0
Estifanos <estifanosbeyene21@gmail.com>
2023-02-02 07:28:17 -0500

    feat: add keymap for scrolling wrapped lines

--- config/nvim/after/ftplugin/markdown.lua
+++ config/nvim/after/ftplugin/markdown.lua
@@ -4,6 +4,9 @@ vim.opt_local.wrap = true
 local buf_map = require("core.utils").buf_map
 local run_file = require("core.utils").run_file
 
+buf_map("n", "j", "gj", { desc = "move down wrapped lines" })
+buf_map("n", "k", "gk", { desc = "move up wrapped lines" })
+
 buf_map("n", "<leader>rr", function()
 	run_file()
 end, { desc = "preview file using glow" })
