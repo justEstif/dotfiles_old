da00e00161df8a152588cbeb654ee90e30150dd7
Estifanos <estifanosbeyene21@gmail.com>
2023-02-08 03:46:12 -0500

    feat: remove unused autocmds

--- config/nvim/lua/core/autocmds.lua
+++ config/nvim/lua/core/autocmds.lua
@@ -1,7 +1,5 @@
-local autogroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 
--- Highlight on yank
 autocmd("TextYankPost", {
 	desc = "highlight on yank",
 	callback = function()
@@ -10,35 +8,8 @@ autocmd("TextYankPost", {
 })
 
 autocmd("BufEnter", {
+	desc = "Disable New Line Comment",
 	callback = function()
 		vim.opt.formatoptions:remove({ "c", "r", "o" })
 	end,
-	desc = "Disable New Line Comment",
-})
-
--- autocmd("CmdWinEnter", {
--- 	pattern = "*",
--- 	callback = function()
--- 		require("cmp").setup({ enabled = false })
--- 	end,
--- })
-
--- autocmd("CmdWinLeave", {
--- 	pattern = "*",
--- 	callback = function()
--- 		require("cmp").setup({ enabled = true })
--- 	end,
--- })
-
--- autocmd("CmdlineEnter", {
--- 	command = "command! Term :botright vsplit term://$SHELL",
--- })
-
-autocmd("TermOpen", {
-	callback = function()
-		vim.opt_local.relativenumber = false
-		vim.opt_local.number = false
-		vim.opt_local.cursorline = false
-	end,
-	desc = "Terminal Options",
 })
