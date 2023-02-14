9435dc1771637f0b5275f40f654bff92bc286762
Estifanos <estifanosbeyene21@gmail.com>
2023-02-14 05:02:21 -0500

    feat: add conditional keymap for quit or bd

--- config/nvim/lua/core/keymaps.lua
+++ config/nvim/lua/core/keymaps.lua
@@ -15,16 +15,24 @@ end, { desc = "clear highlights" })
 
 map("n", "]b", ":bnext<CR>", { desc = "navigate buffers" })
 map("n", "[b", ":bprevious<CR>", { desc = "navigate buffers" })
-map("n", "'", "`", { desc = "jump to mark using '" })
-
 map("n", "<leader>cd", ":tcd %:p:h<cr>", { desc = "change cwd" })
+-- if one buffer quit, else close buffer
+vim.cmd([[
+  func  Smart_qq()
+    if expand('%') == '' && ( len( filter( range(1, bufnr('$')),  'buflisted(v:val)' ) )  == 1 )
+      exe 'q'
+    else
+      exe 'bd'
+    endif
+  endfunc
+  nn  <leader>q :call Smart_qq()<cr>
+]])
 
 -- NvimTree
 map("n", "<leader>f", ":NvimTreeToggle<CR>")
 
 -- Telescope
 map("n", "<F1>", ":Telescope help_tags<cr>", { desc = "help tags" })
-map("n", "<leader>b", ":Telescope buffers<cr>", { desc = "List open buffers" })
 map("n", "<leader>s", ":Telescope live_grep<CR>", { desc = "Search in cwd" })
 map("n", "<leader><leader>", ":Telescope find_files<CR>", { desc = "Find files in cwd" })
 map("n", "z=", ":Telescope spell_suggest<CR>", { desc = "Spell suggest keybind" })
