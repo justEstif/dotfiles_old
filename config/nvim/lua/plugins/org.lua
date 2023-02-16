50965008a225ed8e00ee6050cbb124ffb719e001
Estifanos <estifanosbeyene21@gmail.com>
2023-02-16 05:54:12 -0500

    feat: change org directory and todo alias

--- config/nvim/lua/plugins/org.lua
+++ config/nvim/lua/plugins/org.lua
@@ -6,6 +6,10 @@ end
 
 orgmode.setup_ts_grammar()
 orgmode.setup({
-	org_agenda_files = { "~/notes/**/*" },
-	org_default_notes_file = "~/notes/",
+	org_agenda_files = { "~/zk-notes/org/**/*" },
+	org_default_notes_file = "~/zk-notes/org",
 })
+
+-- Open agenda prompt: <Leader>oa
+-- Open capture prompt: <Leader>oc
+-- In any orgmode buffer press g? for help
