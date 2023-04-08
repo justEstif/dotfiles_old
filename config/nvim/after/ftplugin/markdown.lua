898da70ca2d34a447fbb8a915cce2e96346b5cbc
Estifanos <estifanosbeyene21@gmail.com>
2023-04-08 19:27:40 -0400

    feat: add more qutebrowser adblocks

--- config/nvim/after/ftplugin/markdown.lua
+++ config/nvim/after/ftplugin/markdown.lua
@@ -10,9 +10,11 @@ buf_map("n", "k", "gk", { desc = "move up wrapped lines" })
 
 local function toggle_checkbox()
 	local line = vim.api.nvim_get_current_line()
-	local pattern = "%[([ xX])%]"
+	local pattern = "%[([ -xX])%]"
 	local checkbox = line:match(pattern)
 	if checkbox == " " or not checkbox then
+		line = line:gsub(pattern, "[-]")
+  elseif checkbox == "-" then
 		line = line:gsub(pattern, "[x]")
 	else
 		line = line:gsub(pattern, "[ ]")
--- config/qutebrowser/config.py
+++ config/qutebrowser/config.py
@@ -43,3 +43,26 @@ config.set('content.javascript.enabled', True, 'chrome-devtools://*')
 config.set('content.javascript.enabled', True, 'devtools://*')
 config.set('content.javascript.enabled', True, 'chrome://*/*')
 config.set('content.javascript.enabled', True, 'qute://*/*')
+
+# Adblock
+c.content.blocking.method = 'adblock'
+c.content.blocking.adblock.lists = [
+    "https://easylist.to/easylist/easylist.txt",
+    "https://easylist.to/easylist/easyprivacy.txt",
+    "https://easylist.to/easylist/fanboy-social.txt",
+    "https://secure.fanboy.co.nz/fanboy-annoyance.txt",
+    "https://easylist-downloads.adblockplus.org/abp-filters-anti-cv.txt",
+    # "https://gitlab.com/curben/urlhaus-filter/-/raw/master/urlhaus-filter.txt",
+    "https://pgl.yoyo.org/adservers/serverlist.php?showintro=0;hostformat=hosts",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/legacy.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2020.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2021.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/badware.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/privacy.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/badlists.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/annoyances.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/resource-abuse.txt",
+    "https://www.i-dont-care-about-cookies.eu/abp/",
+    "https://secure.fanboy.co.nz/fanboy-cookiemonster.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/unbreak.txt"]
