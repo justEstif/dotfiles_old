dab7f48126fad99ce15883a7d264456de708f2b1
Estifanos <estifanosbeyene21@gmail.com>
2023-04-20 05:19:52 -0400

    feat: change fish theme from tokyonight to catppuccin

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -9,10 +9,6 @@ set fish_greeting # Turn off the fish welcome message
 set fzf_directory_opts --bind "ctrl-o:execute($EDITOR {} &> /dev/tty)"
 
 ###### Themes ######
-. ~/dotfiles/config/fish/themes/tokyonight_storm.fish
-## . ~/dotfiles/config/fish/themes/tokyonight_day.fish
-## . ~/dotfiles/config/fish/themes/tokyonight_moon.fish
-## . ~/dotfiles/config/fish/themes/tokyonight_night.fish
 
 ###### Alias ######
 alias cdu="cd -" # go to the prev dir
@@ -31,7 +27,42 @@ alias l="exa -lba" # ls
 alias ls="exa" # ls
 alias la='exa -albF --git' # list, size, type, git
 
-alias ni="pnpm i"
-alias nid="pnpm i --save-dev"
-alias nr="pnpm run"
-alias nig="pnpm i -g"
+## alias ni="pnpm i"
+## alias nid="pnpm i --save-dev"
+## alias nr="pnpm run"
+## alias nig="pnpm i -g"
+
+alias p="pnpm"
+alias pa="pnpm add"
+alias pad="pnpm add --save-dev"
+alias pap="pnpm add --save-peer"
+alias pau="pnpm audit"
+alias pb="pnpm run build"
+alias pc="pnpm create"
+alias pd="pnpm run dev"
+alias pdoc="pnpm run doc"
+alias pdocs="pnpm run docs"
+alias pga="pnpm add --global"
+alias pgls="pnpm list --global"
+alias pgrm="pnpm remove --global"
+alias pgu="pnpm update --global"
+alias ph="pnpm help"
+alias pi="pnpm init"
+alias pin="pnpm install"
+alias pln="pnpm run lint"
+alias pls="pnpm list"
+alias pout="pnpm outdated"
+alias ppub="pnpm publish"
+alias prm="pnpm remove"
+alias prun="pnpm run"
+alias ps="pnpm run serve"
+alias pst="pnpm start"
+alias psv="pnpm server"
+alias pt="pnpm test"
+alias ptc="pnpm test --coverage"
+alias pui="pnpm update --interactive"
+alias puil="pnpm update --interactive --latest"
+alias pun="pnpm uninstall"
+alias pup="pnpm update"
+alias pwhy="pnpm why"
+alias px="pnpx"
