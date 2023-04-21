fd0f460df78939db8829c2c32a161173a70477b9
Estifanos <estifanosbeyene21@gmail.com>
2023-04-21 06:14:55 -0400

    feat: remove jinja

--- config/nvim/lua/packer_init.lua
+++ config/nvim/lua/packer_init.lua
@@ -140,8 +140,6 @@ return packer.startup(function(use)
 		end,
 	})
 
-	use("glench/vim-jinja2-syntax")
-
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
 		require("packer").sync()
