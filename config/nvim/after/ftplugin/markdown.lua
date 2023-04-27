3b0d016014c7bdf84aa82cdaddd0c47b47f167b3
Estifanos <estifanosbeyene21@gmail.com>
2023-04-27 07:26:53 -0400

    feat: enable joinspaces in md

--- config/nvim/after/ftplugin/markdown.lua
+++ config/nvim/after/ftplugin/markdown.lua
@@ -1,6 +1,8 @@
 vim.opt_local.spell = true -- enable spell check
 vim.opt_local.wrap = true
 vim.opt_local.linebreak = true -- break at words
+vim.opt_local.joinspaces = true -- autoinsert two spaces after '.', '?', '!' for join command.
+
 vim.b.miniindentscope_disable = true
 
 local buf_map = require("core.utils").buf_map
